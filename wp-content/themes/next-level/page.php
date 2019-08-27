a37d70934a7a76d1f519fcc60a7522bfa374abe1
Morgan Segura <morgansegura@gmail.com>
2019-08-27 08:50:46 -0700

    First commit track my necessary files

---
+++ wp-content/themes/next-level/page.php
@@ -0,0 +1,12 @@
+<?php
+/* 
+    Template Name: Page
+*/
+
+get_header(); ?>
+
+<!-- [start] Content -->
+
+<!-- [end] Content -->
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/next-level/single.php
@@ -0,0 +1,12 @@
+<?php
+/* 
+    Template Name: Single
+*/
+
+get_header(); ?>
+
+<!-- [start] Content -->
+
+<!-- [end] Content -->
+
+<?php get_footer(); ?>
