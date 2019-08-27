5f64ab4f44e9b7d2753b9cdc50772ddddc8b9942
Vy Nguyen <vy.t.nguyen.11@gmail.com>
2019-08-27 16:47:52 -0700

    add main

--- wp-content/themes/nextlevel/front-page.php
+++ wp-content/themes/nextlevel/front-page.php
@@ -9,34 +9,7 @@ get_header(); ?>
 
 <h1><?php the_title(); ?></h1>
 
-<!-- [end] Content -->
-
-<?php get_footer(); ?>
-
-
-
-<!DOCTYPE HTML>
-<!--
-	Caminar by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
--->
-<html>
-	<head>
-		<title>Caminar by TEMPLATED</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link rel="stylesheet" href="assets/css/main.css" />
-	</head>
-	<body>
-
-		<!-- Header -->
-			<header id="header">
-				<div class="logo"><a href="#">Caminar <span>by TEMPLATED</span></a></div>
-			</header>
-
-		<!-- Main -->
-			<section id="main">
+<section id="main">
 				<div class="inner">
 
 				<!-- One -->
@@ -115,27 +88,7 @@ get_header(); ?>
 				</div>
 			</section>
 
-		<!-- Footer -->
-			<footer id="footer">
-				<div class="container">
-					<ul class="icons">
-						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-						<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
-					</ul>
-				</div>
-				<div class="copyright">
-					&copy; Untitled. All rights reserved. Images <a href="https://unsplash.com">Unsplash</a> Design <a href="https://templated.co">TEMPLATED</a>
-				</div>
-			</footer>
+<!-- [end] Content -->
 
-		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.poptrox.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
+<?php get_footer(); ?>
 
-	</body>
-</html>
