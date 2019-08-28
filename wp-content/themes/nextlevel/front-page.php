74ff4a9d550f3731d91b4e3c0a5638a20f4d67ac
Vy Nguyen <vy.t.nguyen.11@gmail.com>
2019-08-27 17:26:41 -0700

    assets

--- wp-content/themes/nextlevel/front-page.php
+++ wp-content/themes/nextlevel/front-page.php
@@ -36,22 +36,22 @@ get_header(); ?>
 					<div class="gallery">
 						<div>
 							<div class="image fit flush">
-								<a href="images/pic03.jpg"><img src="images/pic03.jpg" alt="" /></a>
+								<a href="images/pic03.jpg"><img src="<?php echo get_stylesheet_uri(); ?>/images/pic03.jpg" alt="" /></a>
 							</div>
 						</div>
 						<div>
 							<div class="image fit flush">
-								<a href="images/pic01.jpg"><img src="images/pic01.jpg" alt="" /></a>
+								<a href="images/pic01.jpg"><img src="<?php echo get_stylesheet_uri(); ?>/images/pic01.jpg" alt="" /></a>
 							</div>
 						</div>
 						<div>
 							<div class="image fit flush">
-								<a href="images/pic04.jpg"><img src="images/pic04.jpg" alt="" /></a>
+								<a href="images/pic04.jpg"><img src="<?php echo get_stylesheet_uri(); ?>/images/pic04.jpg" alt="" /></a>
 							</div>
 						</div>
 						<div>
 							<div class="image fit flush">
-								<a href="images/pic05.jpg"><img src="images/pic05.jpg" alt="" /></a>
+								<a href="images/pic05.jpg"><img src="<?php echo get_stylesheet_uri(); ?>/images/pic05.jpg" alt="" /></a>
 							</div>
 						</div>
 					</div>
@@ -61,21 +61,21 @@ get_header(); ?>
 		<!-- Three -->
 			<section id="three" class="wrapper">
 				<div class="spotlight">
-					<div class="image flush"><img src="images/pic06.jpg" alt="" /></div>
+					<div class="image flush"><img src="<?php echo get_stylesheet_uri(); ?>/images/pic06.jpg" alt="" /></div>
 					<div class="inner">
 						<h3>Viverra Hendrerit</h3>
 						<p>Curabitur eget semper ante. Morbi eleifend ultricies est, a blandit diam vehicula vel. Vestibulum porttitor nisi quis viverra hendrerit. Suspendisse vel volutpat nibh, vel elementum lacus. Maecenas commodo pulvinar dui, at cursus metus imperdiet vel.</p>
 					</div>
 				</div>
 				<div class="spotlight alt">
-					<div class="image flush"><img src="images/pic07.jpg" alt="" /></div>
+					<div class="image flush"><img src="<?php echo get_stylesheet_uri(); ?>/images/pic07.jpg" alt="" /></div>
 					<div  class="inner">
 						<h3>Curabitur Eget</h3>
 						<p>Curabitur eget semper ante. Morbi eleifend ultricies est, a blandit diam vehicula vel. Vestibulum porttitor nisi quis viverra hendrerit. Suspendisse vel volutpat nibh, vel elementum lacus. Maecenas commodo pulvinar dui, at cursus metus imperdiet vel.</p>
 					</div>
 				</div>
 				<div class="spotlight">
-					<div class="image flush"><img src="images/pic08.jpg" alt="" /></div>
+					<div class="image flush"><img src="<?php echo get_stylesheet_uri(); ?>/images/pic08.jpg" alt="" /></div>
 					<div class="inner">
 						<h3>Morbi Eleifend</h3>
 						<p>Curabitur eget semper ante. Morbi eleifend ultricies est, a blandit diam vehicula vel. Vestibulum porttitor nisi quis viverra hendrerit. Suspendisse vel volutpat nibh, vel elementum lacus. Maecenas commodo pulvinar dui, at cursus metus imperdiet vel.</p>
--- wp-content/themes/nextlevel/header.php
+++ wp-content/themes/nextlevel/header.php
@@ -4,7 +4,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta charset="<?php bloginfo('charset'); ?>" >
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-  <link rel="stylesheet" href="assets/css/main.css" />
+  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>/assets/css/main.css" />
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
