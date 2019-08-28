1f23d313737ed437c4ac17dbdc3da23d5a760c05
Vy Nguyen <vy.t.nguyen.11@gmail.com>
2019-08-27 21:40:36 -0700

    archive and blog but coming back to blog

--- wp-content/themes/nextlevel/archive.php
+++ wp-content/themes/nextlevel/archive.php
@@ -1,11 +1,40 @@
 <?php
-/* 
+/*
     Template Name: Archives
 */
 
 get_header(); ?>
 
 <!-- [start] Content -->
+        <section id="main">
+		    <div class="inner">
+
+			    <section id="one" class="wrapper style1">
+
+                    <p><strong>By Date</strong></p>
+                    <ul class="archive-cat">
+                        <?php compact_archive($style='block'); ?>
+                    </ul>
+
+                    <p><strong>Categories:</strong></p>
+                    <ul class="archive-cat">
+                        <?php wp_list_categories('title_li='); ?>
+                    </ul>
+
+                    <p><strong>List of Pages:</strong></p>
+                    <ul class="archive-cat">
+                        <?php wp_list_pages( 'title_li=' ); ?>
+                    </ul>
+
+                    <p><strong>List of Authors:</strong></p>
+                    <ul class="archive-cat">
+                        <?php wp_list_authors( 'exclude_admin=0&optioncount=1' ); ?>
+                    </ul>
+
+            <!-- <div class="clear"></div> -->
+</section>
+            </div>
+    </section>
 
 <!-- [end] Content -->
 
---
+++ wp-content/themes/nextlevel/blog.php
@@ -0,0 +1,14 @@
+<?php
+/*
+    Template Name: Blog
+*/
+
+get_header(); ?>
+
+<!-- [start] Content -->
+
+<h1><?php the_title(); ?></h1>
+
+<!-- [end] Content -->
+
+<?php get_footer(); ?>
